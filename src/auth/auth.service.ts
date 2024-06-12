8b78ed046f25d3a7aaa60aeec3e8b48172bea7e0
thiagoassisk8 <tyagosilva82@gmail.com>
2024-06-12 09:33:40 -0300

    barebones auth implemented

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { AuthPayloadDto } from './dto/auth.dto';
+import { JwtService } from '@nestjs/jwt';
+
+const mockdata = [
+  {
+    id: 1,
+    username: 'thiago',
+    password: 'password',    
+  },
+  {
+    id: 2,
+    username: 'assis',
+    password: 'password',
+  },
+];
+
+@Injectable()
+export class AuthService {
+  constructor(private jwtService: JwtService) {}
+
+  validateUser({ username, password }: AuthPayloadDto) {
+    const findUser = mockdata.find((user) => user.username === username);
+    if (!findUser) return null;
+    if (password === findUser.password) {
+      const { password, ...user } = findUser;
+      return this.jwtService.sign(user);
+    }
+  }
+}
