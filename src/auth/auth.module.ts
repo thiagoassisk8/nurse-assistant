264b6881dcb12f8cde241483e2ab9cbf1642347d
thiagoassisk8 <tyagosilva82@gmail.com>
2024-06-16 14:34:24 -0300

    att

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,7 +6,7 @@ import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './strategies/local.strategy';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from '../entities/user.entity'; // Atualize o caminho conforme necessário
+import { User } from '../entities/user.entity'; 
 
 @Module({
   imports: [
